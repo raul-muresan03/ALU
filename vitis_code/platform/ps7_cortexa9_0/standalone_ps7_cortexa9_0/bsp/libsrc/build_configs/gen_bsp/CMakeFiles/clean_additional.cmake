9f705152bc9314d3c093590eb7fddb9a2172971e
raul <raulmuresancalin@gmail.com>
2024-12-23 18:57:36 +0200

    The last modifications and created the platform in Vitis

---
+++ vitis_code/platform/ps7_cortexa9_0/standalone_ps7_cortexa9_0/bsp/libsrc/build_configs/gen_bsp/CMakeFiles/clean_additional.cmake
@@ -0,0 +1,11 @@
+# Additional clean files
+cmake_minimum_required(VERSION 3.16)
+
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "")
+  file(REMOVE_RECURSE
+  "C:\\Users\\Raul\\Desktop\\SSC_projects\\ALU\\vitis_code\\platform\\ps7_cortexa9_0\\standalone_ps7_cortexa9_0\\bsp\\include\\sleep.h"
+  "C:\\Users\\Raul\\Desktop\\SSC_projects\\ALU\\vitis_code\\platform\\ps7_cortexa9_0\\standalone_ps7_cortexa9_0\\bsp\\include\\xiltimer.h"
+  "C:\\Users\\Raul\\Desktop\\SSC_projects\\ALU\\vitis_code\\platform\\ps7_cortexa9_0\\standalone_ps7_cortexa9_0\\bsp\\include\\xtimer_config.h"
+  "C:\\Users\\Raul\\Desktop\\SSC_projects\\ALU\\vitis_code\\platform\\ps7_cortexa9_0\\standalone_ps7_cortexa9_0\\bsp\\lib\\libxiltimer.a"
+  )
+endif()
